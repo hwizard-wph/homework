cca7b2e730c06805aa2762ba9b88f4228ba163ef
hwizard-wph <wengpuhengwph@126.com>
2020-06-03 10:08:21 +0800

    fixed

--- initialize.cpp
+++ initialize.cpp
@@ -1,96 +1,10 @@
-﻿#define C_NONE 0//棋子：黑子,白子,无子
-
-#define C_BLACK 1
-
-#define C_WHITE 2
-
-
-
-//棋型代号 下标 权重
-
-#define OTHER 0//0,其他棋型不考虑
-
-#define WIN 1//100000,白赢
-
-#define LOSE 2//-10000000
-
-#define FLEX4 3//50000,白活4
-
-#define flex4 4//-80000
-
-#define BLOCK4 5//400
-
-#define block4 6//-80000
-
-#define FLEX3 7//400
-
-#define flex3 8//-8000
-
-#define BLOCK3 9//20
-
-#define block3 10//-40
-
-#define FLEX2 11//20
-
-#define flex2 12//-40
-
-#define BLOCK2 13//1
-
-#define block2 14//-2
-
-#define FLEX1 15//1
-
-#define flex1 16//-2
-
-#define C_NONE 0//棋子：黑子,白子,无子
-
-#define C_BLACK 1
-
-#define C_WHITE 2
-
-
-
-	//棋型代号 下标 权重
-
-#define OTHER 0//0,其他棋型不考虑
-
-#define WIN 1//100000,白赢
-
-#define LOSE 2//-10000000
-
-#define FLEX4 3//50000,白活4
-
-#define flex4 4//-80000
-
-#define BLOCK4 5//400
-
-#define block4 6//-80000
-
-#define FLEX3 7//400
-
-#define flex3 8//-8000
-
-#define BLOCK3 9//20
-
-#define block3 10//-40
-
-#define FLEX2 11//20
-
-#define flex2 12//-40
-
-#define BLOCK2 13//1
-
-#define block2 14//-2
-
-#define FLEX1 15//1
-
-#define flex1 16//-2
-	
+﻿
 #include<iostream>
 using namespace std;
+
 int type[4][4][4][4][4][4];
 
-void chess::init_type() {
+void chess::initType() {
 	memset(type, 0, sizeof(type));
 	type[2][2][2][2][2][2] = WIN;
 	type[2][2][2][2][2][0] = WIN;
@@ -111,7 +25,7 @@ void chess::init_type() {
 	type[0][2][2][2][2][0] = FLEX4;
 	//黑活4
 	type[0][1][1][1][1][0] = flex4;
-//白活3
+	//白活3
 	type[0][2][2][2][0][0] = FLEX3;
 	type[0][0][2][2][2][0] = FLEX3;
 	type[0][2][0][2][2][0] = FLEX3;
@@ -174,114 +88,102 @@ void chess::init_type() {
 
 							if (p1 == 3 || p6 == 3) {//有边界
 								if (p1 == 3 && p6 != 3) {//左边界
-								//白冲4
+									//白冲4
 									if (ix == 0 && iy == 4) {//若右边有活4无妨，因为活4的权重远大于冲4
-										if (type[p1][p2][p3][p4][p5][6] == 0)
+										if (type[p1][p2][p3][p4][p5][p6] == 0)
 											type[p1][p2][p3][p4][p5][p6] = BLOCK4;
 
 									}
 									//黑冲4
 									if (ix == 4 && iy == 0) {
-										if (type[p1][p2][p3][p4][p5][6] == 0)
-											type[p1][p2][p3][p4][p5][6] == block4;
+										if (type[p1][p2][p3][p4][p5][p6] == 0)
+											type[p1][p2][p3][p4][p5][p6] = block4;
 
 									}
 									//白眠3
 									if (ix == 0 && iy == 3) {
-										if (type[p1][p2][p3][p4][p5][6] == 0)
-											type[p1][p2][p3][p4][p5][6] == BLOCK3;
+										if (type[p1][p2][p3][p4][p5][p6] == 0)
+											type[p1][p2][p3][p4][p5][p6] = BLOCK3;
 									}
 									//黑眠3
 									if (ix == 3 && iy == 0) {
-										if (type[p1][p2][p3][p4][p5][6] == 0)
-											type[p1][p2][p3][p4][p5][6] == block3;
+										if (type[p1][p2][p3][p4][p5][p6] == 0)
+											type[p1][p2][p3][p4][p5][p6] = block3;
 									}
 									//白眠2
 									if (ix == 0 && iy == 2) {
-										if (type[p1][p2][p3][p4][p5][6] == 0)
-											type[p1][p2][p3][p4][p5][6] == BLOCK2;
+										if (type[p1][p2][p3][p4][p5][p6] == 0)
+											type[p1][p2][p3][p4][p5][p6] = BLOCK2;
 									}
-
-
-
-
-								}
-								else if (p6 == 3 && p1 != 3) {//有边界
+								}	else if (p6 == 3 && p1 != 3) {//有边界
 									//白冲4
 									if (x == 0 && y == 4) {
-										if (type[p1][p2][p3][p4][p5][6] == 0)
-											type[p1][p2][p3][p4][p5][6] == BLOCK4;
+										if (type[p1][p2][p3][p4][p5][p6] == 0)
+											type[p1][p2][p3][p4][p5][p6] = BLOCK4;
 
 									}
 									//黑冲4
 									if (x == 4 && y == 0) {
-										if (type[p1][p2][p3][p4][p5][6] == 0)
-											type[p1][p2][p3][p4][p5][6] == block4;
-
-
+										if (type[p1][p2][p3][p4][p5][p6] == 0)
+											type[p1][p2][p3][p4][p5][p6] = block4;
 									}
 									//黑眠3
 									if (x == 3 && y == 0) {
-										if (type[p1][p2][p3][p4][p5][6] == 0)
-											type[p1][p2][p3][p4][p5][6] == BLOCK3;
+										if (type[p1][p2][p3][p4][p5][p6] == 0)
+											type[p1][p2][p3][p4][p5][p6] = BLOCK3;
 									}
 									//白眠3
 									if (x == 0 && y == 3) {
-										if (type[p1][p2][p3][p4][p5][6] == 0)
-											type[p1][p2][p3][p4][p5][6] == block3;
+										if (type[p1][p2][p3][p4][p5][p6] == 0)
+											type[p1][p2][p3][p4][p5][p6] = block3;
 									}
 									//黑眠2
 									if (x == 2 && y == 0) {
-										if (type[p1][p2][p3][p4][p5][6] == 0)
-											type[p1][p2][p3][p4][p5][6] == BLOCK2;
+										if (type[p1][p2][p3][p4][p5][p6] == 0)
+											type[p1][p2][p3][p4][p5][p6] = BLOCK2;
 									}
 									//白眠2
 									if (x == 0 && y == 2) {
-										if (type[p1][p2][p3][p4][p5][6] == 0)
-											type[p1][p2][p3][p4][p5][6] == block2;
+										if (type[p1][p2][p3][p4][p5][p6] = 0)
+											type[p1][p2][p3][p4][p5][p6] = block2;
 									}
-
-
 								}
-							}
-							else {//无边界
+							}	else {//无边界
 								//白冲4
 								if ((x == 0 && y == 4) || (ix == 0 && iy == 4)) {
-									if (type[p1][p2][p3][p4][p5][6] == 0)
-										type[p1][p2][p3][p4][p5][6] == BLOCK4;
+									if (type[p1][p2][p3][p4][p5][p6] == 0)
+										type[p1][p2][p3][p4][p5][p6] = BLOCK4;
 								}
 								//黑冲4
 								if ((x == 4 && y == 0) || (ix == 4 && iy == 0)) {
-									if (type[p1][p2][p3][p4][p5][6] == 0)
-										type[p1][p2][p3][p4][p5][6] == block4;
+									if (type[p1][p2][p3][p4][p5][p6] == 0)
+										type[p1][p2][p3][p4][p5][p6] = block4;
 								}
 								//白眠3
 								if ((x == 0 && y == 3) || (ix == 0 && iy == 3)) {
-									if (type[p1][p2][p3][p4][p5][6] == 0)
-										type[p1][p2][p3][p4][p5][6] == BLOCK3;
+									if (type[p1][p2][p3][p4][p5][p6] == 0)
+										type[p1][p2][p3][p4][p5][p6] = BLOCK3;
 								}
 								//黑眠3
 								if ((x == 3 && y == 0) || (ix == 3 && iy == 0)) {
-									if (type[p1][p2][p3][p4][p5][6] == 0)
-										type[p1][p2][p3][p4][p5][6] == block3;
+									if (type[p1][p2][p3][p4][p5][p6] == 0)
+										type[p1][p2][p3][p4][p5][p6] = block3;
 								}
 								//白眠2
 								if ((x == 0 && y == 2) || (ix == 0 && iy == 2)) {
-									if (type[p1][p2][p3][p4][p5][6] == 0)
-										type[p1][p2][p3][p4][p5][6] == BLOCK2;
+									if (type[p1][p2][p3][p4][p5][p6] == 0)
+										type[p1][p2][p3][p4][p5][p6] = BLOCK2;
 								}
 								//黑眠2
 								if ((x == 2 && y == 0) || (ix == 2 && iy == 0)) {
-									if (type[p1][p2][p3][p4][p5][6] == 0)
-										type[p1][p2][p3][p4][p5][6] == block2;
+									if (type[p1][p2][p3][p4][p5][p6] == 0)
+										type[p1][p2][p3][p4][p5][p6] = block2;
 								}
-
-
 							}
 						}
 					}
 				}
 			}
 		}
-
 	}
+}
